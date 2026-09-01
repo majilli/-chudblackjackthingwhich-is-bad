2ceeaa337baa7ff8109a4eea0a07d6eb2c19df84
mail <ismailportalk@gmail.com>
2026-09-01 16:25:39 -0700

    Update blackjack_ attempt.py

--- blackjack_ attempt.py
+++ blackjack_ attempt.py
@@ -1,20 +1,24 @@
 import random
 
+
 while True:
     print("Welcome to blackjack!")
-# Creates the variable hand which gets assigned a random integer between 1 and 11 
-# and is printed to the console so the player knows what card they have been dealt
-
-    Q = int(10)
-    J = int(10)
-    K = int(10)
-    hand = random.randint(1, 11) 
+    # Creates the variable hand which gets assigned a random integer between 1 and 11 
+    # and is printed to the console so the player knows what card they have been dealt
+    face_cards = ["Q", "K", "J"]
+    cards = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    the_cards = cards + face_cards
+    hand = random.choice(the_cards)
     print("Your hand is:", hand)
+    if hand in face_cards:
+            hand = 10
     # Blackjack loop which goes on until the player chooses to stand or busts by going over 21
     while hand < 21:
         hit = input("Do you want to hit? (yes/no): ")
         if hit == "yes":
-            new_card = random.randint(1, 11) 
+            new_card = random.choice(the_cards)
+            if new_card in face_cards:
+                new_card = 10
             hand += new_card
 
             print("You drew:", new_card)
@@ -30,21 +34,26 @@ while True:
         print("You busted!") # The player loses if they bust and go over 21 and the game ends.
 
     else:
-        dealer_hand = random.randint(1,11)         # The dealer's hand is created and assigned a random integer between 1 and 11.
+        dealer_hand = random.choice(the_cards)   
+            # The dealer's hand is created and assigned a random integer between 1 and 11.
         print("Dealer's hand is:", dealer_hand)
-
+        if dealer_hand in face_cards:
+                    dealer_hand = 10
         while dealer_hand < 17:                     # A loop is created so the dealer will keep drawing cards until they reach 17 or higher.
-            new_card = random.randint(1, 11)       # The dealers new card is created and assigned a random integer between 1 and 11.
-            dealer_hand = dealer_hand + new_card     # The dealers new and old cards are added togeher so the player knows the dealers new hand.
+            new_card = random.choice(the_cards)
+                # The dealers new and old cards are added togeher so the player knows the dealers new hand.
             print("Dealer drew:", new_card)
+            if new_card in face_cards:
+             new_card = 10
+            dealer_hand = dealer_hand + new_card
             print("Dealer's new hand is:", dealer_hand)
     
         if dealer_hand > 21:                               # If the dealer busts by going over 21 the player wins and the game ends.
-                    print("Dealer busts! You win!")
+            print("Dealer busts! You win!")
         elif hand > dealer_hand:                  # If the players hand is greater than the dealers hand the player wins and the game ends.
-                    print("You win!")
+            print("You win!")
         elif hand < dealer_hand:                 #If the dealer has a greater hand than the player the dealer wins and the game ends.
-                    print("Dealer wins!")
+            print("Dealer wins!")
         else:                                  # If the player has the same hand as the dealer the game ends in a tie.
             print("It's a tie!")
     
@@ -54,7 +63,6 @@ while True:
         break
 
 
-
     
 
 
@@ -66,24 +74,3 @@ while True:
 
 
 
-##
-
-## print("Welcome to blackjack!")
-
-## hand = random.randint(1, 11)
-## print("Your hand is: ", hand)
-
-## stand = input("Do you want to stand? : ")
-
-## if stand == "yes":
-   ##print("You chose to stand.")
-
-##elif stand == "no":
-   ## hit = input("Do you want to hit? : ")
-
-##
-##if hit == "yes": 
-   ## new_card = random.randint(1, 11)
-   ## hand = hand + new_card
-   ## print("You drew: ", new_card)
-  ##  print("Your new hand is: ", hand)
